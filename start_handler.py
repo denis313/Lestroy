ab524d19402abaa5c6528cc693f63c47994e4cbc
evtus <evtushokdenis2004@gmail.com>
2025-06-02 00:57:47 +0300

    update file

--- start_handler.py
+++ start_handler.py
@@ -63,7 +63,7 @@ async def type_building(message: Message, state: FSMContext):
     await bot.send_message(
         chat_id=message.chat.id,
         text=lexicon['project'],
-            reply_markup=keyboard_project().as_markup(resize_keyboard=True))
+            reply_markup=keyboard_project())
     await state.set_state(Cost.project)
 
 
